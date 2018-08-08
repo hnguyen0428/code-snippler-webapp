df1ab696c77a12b504fea7e5a3bb588dca478798
Hoang Nguyen <hnguyen0428@yahoo.com>
2018-08-07 22:19:31 -0700

    setup react-toolbox

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './assets/react-toolbox/theme.css';
+import theme from './assets/react-toolbox/theme';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import Button from 'react-toolbox/lib/button/Button';
+import Input from 'react-toolbox/lib/input/Input';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+
+                    <Button raised primary>Test Button</Button>
+                    <Input type='text' label='Name' name='name' maxLength={16} />
+                </div>
+            </ThemeProvider>
+        );
+    }
 }
 
 export default App;
